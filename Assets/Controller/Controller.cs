595a885c9f332f7c86bd9f35ad6f18ee3ab39945
zyuq123 <zyuq123@126.com>
2020-06-04 11:33:07 +0800

    MVC例子
    简易的MVC案例

---
+++ Assets/Controller/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller 
+{
+    Player playerModel;
+    PlayerView currentView;
+
+    public Controller() {
+        //1:初始化Model数据
+        playerModel = new Player();
+        //给Model事件添加函数，此函数功能是修改视图显示
+        playerModel.HpChange += UpdateLabel;
+
+        //2:初始化View
+        currentView = GameObject.Find("PlayerView").GetComponent<PlayerView>();
+        //此事件在点击按钮的时候触发
+        currentView.OnClick += (s, e) =>
+        {
+            Debug.Log("Click");
+            playerModel.HP = 50;
+        };
+    }
+
+    public void UpdateLabel(object sender, EventArgs args) {
+        currentView.Text = playerModel.HP.ToString();
+    }
+}
---
+++ Assets/InitController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitController : MonoBehaviour
+{
+    Controller control;
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = new Controller();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
