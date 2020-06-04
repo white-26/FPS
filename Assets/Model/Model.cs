c7998548540226804cf2f26de7594fb9c9d957d2
zyuq123 <zyuq123@126.com>
2020-06-04 11:41:42 +0800

    Revert "Revert "MVC例子""
    This reverts commit a83c7198eafdd0b79b1af8efb102993ddef78645.

---
+++ Assets/Model/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player
+{
+    private int hp;
+    public int HP {
+        get { return hp; }
+        set { 
+            hp += value;
+            if (HpChange!=null)
+            {
+                HpChange(this,new EventArgs());
+            }
+        }
+    }
+    //当玩家的血量进行更新的时候，界面应该发生变化
+    //最传统的方式全部写道一起，获取玩家的Player组件，将组件里面的血量HP--
+
+    //定义一个事件，当血量变化的时候，会触发该事件
+    public event EventHandler HpChange;
+
+}
---
+++ Assets/View/PlayerView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerView : MonoBehaviour
+{
+    public Text hpLabel;//血量标签
+    public Button addButton;//模拟按钮触发修改血量
+
+    public event EventHandler OnClick;
+    private void Start()
+    {
+        addButton.onClick.AddListener(BtnClick);
+    }
+
+    void BtnClick() {
+        if (OnClick !=null)
+        {
+            OnClick(this,new EventArgs());
+        }
+    }
+
+    public string Text {
+        get { return hpLabel.text; }
+        set {
+            hpLabel.text = value;
+        }
+    }
+ 
+}
